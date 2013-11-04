c2a3832079aa81d56775e6adc2994d66813df5ce
rom117 <Romain.Lalande@gmail.com>
2013-11-04 17:06:07 +0100

    add variables until tel

--- crawl_seloger.com.py
+++ crawl_seloger.com.py
@@ -8,6 +8,107 @@ import urllib
 import csv
 
 
+def get_cp(soup):
+	"""
+	FUNCTION
+	get the zip code
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	cp variable [int]
+	"""
+	#<h1 itemprop="name" title="Location Studio | Paris 4ème (75004)">Location Studio<br />Paris 4ème (75004)</h1>
+	cp=soup.find("h1").get_text()
+	return cp[cp.find("(")+1:cp.find(")")]
+
+
+def get_prix(soup):
+	"""
+	FUNCTION
+	get the monthly price
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	prix variable [int]
+	"""
+	#<b class="prix_brut">348&nbsp;&euro;&nbsp;cc<sup>*</sup> </b>
+	return soup.find("b", {"class": "prix_brut"}).get_text().split()[0]
+
+
+def get_piece(soup):
+	"""
+	FUNCTION
+	get the number of rooms
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	piece variable [int]
+	"""
+	return soup[0].find("b").get_text().strip()
+
+
+def get_surf(soup):
+	"""
+	FUNCTION
+	get the surface
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	surf variable [int]
+	"""
+	#<li class="switch_style" title="Surface">Surface <b title="10 m²">    10 m² </b></li>
+	return soup[1].find("b").get_text().split()[0]
+
+
+def get_etg(soup):
+	"""
+	FUNCTION
+	get the number of floors
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	etg variable [int]
+	"""
+	return soup[2].find("b").get_text().strip()
+
+
+def get_asc(soup):
+	"""
+	FUNCTION
+	get the lift data
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	asc variable [int]
+	"""
+	return soup[3].find("b").get_text().strip()
+
+
+def get_ter(soup):
+	"""
+	FUNCTION
+	get the terrace data
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	ter variable [int]
+	"""
+	return soup[4].find("b").get_text().strip()
+
+
+def get_park(soup):
+	"""
+	FUNCTION
+	get the car park data (yes or no)
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	park: park variable [boolean]
+	"""
+	# <li class="switch_style float_right">Parking <b>N.C</b></li>
+	return soup[5].find("b").get_text().strip()
+
+
 def get_maj(soup):
 	"""
 	FUNCTION
@@ -34,17 +135,87 @@ def get_ref(soup):
 	return soup.find("span", {"title": "Référence de l'annonce"}).find("b").get_text().strip()
 
 
-def get_prix(soup):
+def get_transp(soup):
 	"""
 	FUNCTION
-	get the monthly price
+	get the means of transport in the neighborhood
 	PARAMETERS
 	soup: html content of the page [BeautifulSoup object]
 	RETURN
-	prix variable [int]
+	transp variable [int]
 	"""
-	#<b class="prix_brut">348&nbsp;&euro;&nbsp;cc<sup>*</sup> </b>
-	return soup.find("b", {"class": "prix_brut"}).get_text().split()[0]
+	#<dt class="trans_ann">Transports&nbsp;:</dt><dd class="metro_paris" title="Métro Paris">Hotel de Ville</dd>
+	transp=""
+	metro="metro:"+soup.find("dt", {"class": "trans_ann"}).findNext("dd", {"class": "metro_paris"}).get_text().strip()
+	rer="rer:"
+	transp=metro+";"+rer
+	return transp
+
+
+def get_prox(soup):
+	"""
+	FUNCTION
+	get the shops, buildings in the neighborhood
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	prox variable [int]
+	"""
+	#<dt>Proximité :</dt><dd>Métro hôtel de ville commerces</dd>
+	return soup.find("dt", text="Proximité :").findNext("dd").get_text().strip()
+
+
+def get_descr(soup):
+	"""
+	FUNCTION
+	get the description of the ad
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	descr variable [int]
+	"""
+	#<meta name="description" content="Chambre avec coin cuisine, rue du plâtre, au 7ème étage sans ascenseur. WC et douche communs à l'étage." />
+	return soup.find("meta", {"name": "description"})["content"]
+
+
+def get_hon(soup):
+	"""
+	FUNCTION
+	get the price of fees
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	hon variable [int]
+	"""
+	#<li title="Honoraires ttc en sus">Honoraires ttc... <b>356,41&nbsp;&euro;</b></li>
+	return soup.find("li", {"title": "Honoraires ttc en sus"}).find("b").get_text().split()[0]
+
+
+def get_tel(soup):
+	"""
+	FUNCTION
+	get the phone number of the announcer or owner
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	tel variable [string]
+	"""
+	#<div class="tel">01 46 24 48 66</div>
+	return soup.find("div", {"class": "tel"}).get_text().strip()
+
+
+
+
+def get_score(soup):
+	"""
+	FUNCTION
+	get the score of the location (security, reputation, means of transports, quietness)
+	PARAMETERS
+	soup: html content of the page [BeautifulSoup object]
+	RETURN
+	score variable [int]
+	"""
+	return ""
 
 
 def get_chauf(soup):
@@ -71,32 +242,6 @@ def get_cuis(soup):
 	return ""
 
 
-def get_surf(soup):
-	"""
-	FUNCTION
-	get the surface
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	surf variable [int]
-	"""
-	#<li class="switch_style" title="Surface">Surface <b title="10 m²">    10 m² </b></li>
-	return soup.find("li", {"title": "Surface"}).find("b").get_text().split()[0]
-
-
-def get_park(soup):
-	"""
-	FUNCTION
-	get the car park data (yes or no)
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	park: park variable [boolean]
-	"""
-	# <li class="switch_style float_right">Parking <b>N.C</b></li>
-	return soup.find(text=re.compile("Parking")).findNext("b").get_text().strip()
-
-
 def get_gar(soup):
 	"""
 	FUNCTION
@@ -105,18 +250,6 @@ def get_gar(soup):
 	soup: html content of the page [BeautifulSoup object]
 	RETURN
 	gar variable [int]
-	"""
-	return ""
-
-
-def get_hon(soup):
-	"""
-	FUNCTION
-	get the price of fees
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	hon variable [int]
 	"""
 	return ""
 
@@ -133,30 +266,6 @@ def get_disp(soup):
 	return ""
 
 
-def get_ter(soup):
-	"""
-	FUNCTION
-	get the terrace data
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	ter variable [int]
-	"""
-	return ""
-
-
-def get_asc(soup):
-	"""
-	FUNCTION
-	get the lift data
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	asc variable [int]
-	"""
-	return ""
-
-
 def get_sdb(soup):
 	"""
 	FUNCTION
@@ -165,30 +274,6 @@ def get_sdb(soup):
 	soup: html content of the page [BeautifulSoup object]
 	RETURN
 	sdb variable [int]
-	"""
-	return ""
-
-
-def get_etg(soup):
-	"""
-	FUNCTION
-	get the number of floors
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	etg variable [int]
-	"""
-	return ""
-
-
-def get_piece(soup):
-	"""
-	FUNCTION
-	get the number of rooms
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	piece variable [int]
 	"""
 	return ""
 
@@ -265,54 +350,6 @@ def get_ent(soup):
 	return ""
 
 
-def get_cp(soup):
-	"""
-	FUNCTION
-	get the zip code
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	cp variable [int]
-	"""
-	return ""
-
-
-def get_transp(soup):
-	"""
-	FUNCTION
-	get the means of transport in the neighborhood
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	transp variable [int]
-	"""
-	return ""
-
-
-def get_prox(soup):
-	"""
-	FUNCTION
-	get the shops, buildings in the neighborhood
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	prox variable [int]
-	"""
-	return ""
-
-
-def get_descr(soup):
-	"""
-	FUNCTION
-	get the description of the ad
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	descr variable [int]
-	"""
-	return ""
-
-
 def get_calme(soup):
 	"""
 	FUNCTION
@@ -337,42 +374,26 @@ def get_meuble(soup):
 	return ""
 
 
-def get_score(soup):
+
+def get_specific_vars(names, soup_specific):
 	"""
 	FUNCTION
-	get the score of the location (security, reputation, means of transports, quietness)
+	get specific variables cotained in a part of the html content of the page
 	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
+	var_names: list of variables to retrieve [list]
+	soup_specific: part of the html content [BeautifulSoup object]
 	RETURN
-	score variable [int]
+	values of variables included given in parameter [list]
 	"""
-	return ""
-
-
-def get_tel(soup):
-	"""
-	FUNCTION
-	get the phone number of the announcer or owner
-	PARAMETERS
-	soup: html content of the page [BeautifulSoup object]
-	RETURN
-	tel variable [string]
-	"""
-	return ""
-
-
-
-
-def get_headers_rentals():
-	"""
-	FUNCTION
-	get the list of variable names to retrieve
-	PARAMETERS
-	None
-	RETURN
-	list of variable names [list]
-	"""
-	return ["maj", "ref", "prix", "chauf", "cuis", "surf", "park", "gar", "hon", "disp", "ter", "asc", "sdb", "etg", "piece", "rang", "const", "gard", "toil", "toil_sep", "ent", "cp", "transp", "prox", "descr", "calme", "meuble", "score", "tel"]
+	rental=[]
+	for name in names:
+			try:
+				value=eval("get_"+name)(soup_specific)
+				rental.append(value)
+				print name+":", value
+			except Exception, e:
+				print "get_data_rental exception", e
+	return rental
 
 
 def get_data_rental(url=""):
@@ -383,58 +404,79 @@ def get_data_rental(url=""):
 	url: page of the rental to crawl [string]
 	example of page: http://www.seloger.com/annonces/locations/appartement/paris-4eme-75/82466803.htm?cp=75&idqfix=1&idtt=1&idtypebien=1,2)
 	VARIABLES TO RETRIEVE
+	cp: zip postal of the rental
+	prix: price of the rental
+	piece: number of rooms
+	surf: surface
+	etg: number of floors
+	asc: is there a lift?
+	ter: is there a terrace?
+	park: is there a car park?
 	maj: on-line publishing date (or date of update)
 	ref: reference of the rental
-	prix: price of the rental
+	transp: means of transport in the neighborhood
+	prox: shops, buildings in the neighborhood
+	descr: description of the ad
+	hon: price of fees
+	tel: phone number of the announcer or owner
+
+	#TODO
+	score: score of the location (security, reputation, means of transports, quietness)
 	chauf: is there a heater system?
 	cuis: is there a kitchen?
-	surf: surface
-	park: is there a car park?
 	gar: price of the guarantee
-	hon: price of fees
 	disp: availability
-	ter: is there a terrace?
-	asc: is there a lift?
 	sdb: is there a bathroom?
-	etg: number of floors
-	piece: number of rooms
 	rang: rangements
 	const: year of construction
 	gard: is there a watchman?
 	toil: is there one WC?
 	toil_sep: is the WC separated?
 	ent: number of entrances
-	cp: zip postal of the rental
-	transp: means of transport in the neighborhood
-	prox: shops, buildings in the neighborhood
-	descr: description of the ad
 	calme: is it quiet?
 	meuble: are there furniture?
-	score: score of the location (security, reputation, means of transports, quietness)
-	tel: phone number of the announcer or owner
+
 	link: webpage of the rental
 	RETURN
 	rental: retrieved data from a rental page [list]
 	"""
 	rental=[]
 	#TEST (to be removed)
-	url="example.html"
+	url="example1.html"
 	soup = BeautifulSoup(urllib.urlopen(url))
-	#~ maj_ref=soup.find("div", {"class": "maj_ref"})
-	#~ rental["maj"]=get_maj(maj_ref)
-	#~ rental["ref"]=get_ref(maj_ref)
-	headers=get_headers_rentals()
-	for key in headers:
-		try:
-			value=eval("get_"+key)(soup)
-			rental.append(value)
-			print key+":", value
-		except Exception, e:
-			pass
-			#~ print "get_data_rental exception", e
+
+	#cp, prix
+	#<div class="header_ann" itemscope itemtype="http://schema.org/Product">
+	soup_cp_prix=soup.find("div", {"class": "header_ann"})
+	names=["cp", "prix"]
+	rental.extend(get_specific_vars(names, soup_cp_prix))
+
+	#piece, surf, etg, asc, ter, park, maj, ref, transp, prox
+	#<div class="infos_ann_light"><ol class="liste_details">
+	soup_main_vars=soup.find("div", {"class": "infos_ann_light"})
+	#piece, surf, etg, asc, ter, park:
+	#<li class="switch_style">
+	soup_main_info=soup_main_vars.find("ol", {"class": "liste_details"}).find_all("li")
+	names=["piece", "surf", "etg", "asc", "ter", "park"]
+	rental.extend(get_specific_vars(names, soup_main_info))
+	#maj, ref
+	#<div class="maj_ref">
+	soup_maj_ref=soup_main_vars.find("div", {"class": "maj_ref"})
+	names=["maj", "ref"]
+	rental.extend(get_specific_vars(names, soup_maj_ref))
+	#transp, prox
+	#<dl>
+	soup_transp_prox=soup_main_vars.find("dl")
+	names=["transp", "prox"]
+	rental.extend(get_specific_vars(names, soup_transp_prox))
+
+	#other independant variables
+	names=["descr", "hon", "tel"]
+	rental.extend(get_specific_vars(names, soup))
+
 
 	#add the link of the page
-	rental.add(url)
+	rental.append(url)
 
 	return rental
 
@@ -454,6 +496,18 @@ def get_data_rentals():
 	for page in pages:
 		rentals.append(get_data_rental(page))
 	return rentals
+
+
+def get_headers_rentals():
+	"""
+	FUNCTION
+	get the list of variable names to retrieve
+	PARAMETERS
+	None
+	RETURN
+	list of variable names [list]
+	"""
+	return ["cp", "prix", "piece", "surf", "etg", "asc", "ter", "park", "maj", "ref", "transp", "prox", "descr", "hon", "tel", "score", "chauf", "cuis", "gar", "disp", "sdb", "rang", "const", "gard", "toil", "toil_sep", "ent", "calme", "meuble"]
 
 
 def save_data_rentals(path_file, rentals):
